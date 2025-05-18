afa4356fc1b6c2c0b41ff6f8c33dacc5111ba521
apavazza <115418293+apavazza@users.noreply.github.com>
2025-05-18 23:49:41 +0200

    Added version information to footer and adjusted theme switcher button position

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,19 +1,44 @@
 import { Link } from "@/i18n/routing"
 import { useTranslations } from "next-intl"
 
+
 export default function Footer() {
   const t = useTranslations("Footer")
   return (
-    <footer className="bg-blue-950 dark:bg-black text-white py-4">
-      <div className="container mx-auto px-4">
-        <div className="flex flex-col lg:flex-row items-center">
-          <Link
-            href={"/privacy"}
-            className="text-sm text-gray-200 hover:text-gray-300 mb-2 lg:mb-0 lg:absolute lg:left-4"
-          >
-            {t("privacy")}
-          </Link>
-          <p className="text-sm w-full text-center">&copy; 2025 Amadeo Pavazza. {t("notice")}</p>
+    <footer id="page-footer" className="py-2 lg:py-4 relative bg-blue-950 dark:bg-black text-white">
+      <div className="px-4">
+        <div className="flex flex-col lg:flex-row lg:relative items-center">
+          <div className="order-2 lg:mx-auto">
+            <p className="text-sm text-center">
+              &copy; 2025 Amadeo Pavazza. {t("notice")}
+            </p>
+          </div>
+
+          <div className="order-1 lg:absolute lg:left-0">
+            <div className="flex flex-row space-x-5 items-center">
+              <Link
+                href={"/privacy"}
+                className="text-sm text-gray-200 hover:text-gray-300 lg:absolute lg:left-4"
+              >
+                {t("privacy")}
+              </Link>
+            </div>
+          </div>
+
+          <div className="order-3 lg:absolute lg:right-0">
+            <p className="text-sm text-gray-200">
+              {t("version")}: {process.env.NEXT_PUBLIC_GIT_HASH ? (
+                <Link
+                  href={`https://github.com/apavazza/personal-website/commits/${process.env.NEXT_PUBLIC_GIT_HASH}`}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="hover:text-gray-300"
+                >
+                  {process.env.NEXT_PUBLIC_GIT_HASH}
+                </Link>
+              ) : "development"}
+            </p>
+          </div>
         </div>
       </div>
     </footer>
